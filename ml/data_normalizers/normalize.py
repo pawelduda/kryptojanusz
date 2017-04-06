7453fd050cb24d718a9be4bda3ce09ada3e26d6a
pawelduda <pawelduda91@gmail.com>
2017-04-06 18:18:45 +0200

    Add weighted average

--- ml/data_normalizers/normalize.py
+++ ml/data_normalizers/normalize.py
@@ -1,7 +1,9 @@
 import numpy
+import talib
 
 from features.simple_10_day_moving_average import simple_10_day_moving_average
 
+# TODO: ensure that weightedAverage is calculated based on last 10 days
 # https://www.researchgate.net/publication/222043783_Predicting_direction_of_stock_price_index_movement_using_artificial_neural_networks_and_support_vector_machines_The_sample_of_the_Istanbul_Stock_Exchange
 # http://ta-lib.org/function.html
 # Features:
@@ -15,6 +17,11 @@ from features.simple_10_day_moving_average import simple_10_day_moving_average
 # [ ] Larry William's R%
 # [ ] Accumulation/Distribution Oscillator
 # [ ] Commodity Channel Index
+
+# close = numpy.random.random(100)
+# print(close)
+# output = talib.CCI(close)
+# print(output)
 
 input_btc_eth_file = open('../btc_historical_data/data/BTC_ETH.csv')
 input_btc_eth_file.readline()
@@ -53,10 +60,11 @@ for i, row in enumerate(data[1:]):
 
     output_file.write(
         # weightedAverage, change (-1 if decrease, 0 if equal, 1 if increase)
-        '{0},{1},{2}\n'.format(
+        '{0},{1},{2},{3}\n'.format(
             data[i - 1][-1],
             data[i][-1],
-            ten_day_moving_average
+            ten_day_moving_average,
+            data[i][7] # weightedAverage
         )
     )
 
