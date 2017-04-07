8a0ed46c4f201cf9896fa20004c6666c7448e6e1
pawelduda <pawelduda91@gmail.com>
2017-04-07 18:31:54 +0200

    Mess around with different data (~51% accurracy)

--- ml/svm.py
+++ ml/svm.py
@@ -1,6 +1,7 @@
+import sklearn.metrics as metrics
 from sklearn import svm
 from sklearn.externals import joblib
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 import numpy
 
 input_btc_eth_file = open('../btc_historical_data/normalized_data/BTC_ETH_normalized.csv')
@@ -11,15 +12,13 @@ data = numpy.loadtxt(
     dtype='float'
 )
 
-features = [row[1:4] for row in data]
-targets = [row[13] for row in data]
+features = [row[4:6] for row in data[0:10000]]
+targets = [row[13] for row in data[0:10000]]
 
 X_train, X_test, y_train, y_test = train_test_split(
     features, targets, test_size=0.4, random_state=0
 )
 
-# X = [row[1:4] for row in data[0:10000]]
-# y = [row[13] for row in data[0:10000]]
 
 # clf = svm.SVC(kernel='poly', verbose=3)
 # print(clf.fit(X_train, y_train))
@@ -28,8 +27,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 clf = joblib.load('clf.pkl')
 
-# print(clf.predict([row[1:4] for row in data[4000:5000]]))
-print(clf.score(X_test, y_test))
+# cross_val_score(clf, features, targets, cv=5))
+predicted = clf.predict([row[4:6] for row in data[0:150000]])
+# predicted = cross_val_predict(clf, [row[4:6] for row in data[0:100]], [row[13] for row in data[0:100]], cv=5)
+print(metrics.accuracy_score([row[13] for row in data[0:150000]], predicted))
+
+
+# print(clf.score(X_test, y_test))
 
 # TODO: sample the dataset
 # TODO: visualise the results on plot
