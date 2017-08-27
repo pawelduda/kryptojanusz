72dc81a94f36dc4e530958c4f50f78c22e5b59c9
pawelduda <pawelduda91@gmail.com>
2017-08-27 12:36:20 +0200

    Consider latest tick in signal generation

--- v2/predictor_bittrex.py
+++ v2/predictor_bittrex.py
@@ -5,14 +5,17 @@ DECIDE_TO_BUY = 1
 DECIDE_TO_HOLD = 0
 DECIDE_TO_SELL = -1
 
-def predict_newest(chart_data):
-    dataframe = pd.read_json(chart_data, convert_dates=True)
-    dataframe['T'] = pd.to_datetime(dataframe['T'], utc=True)
+def predict_newest(market_ticks, latest_tick):
+    market_ticks = pd.read_json(market_ticks, convert_dates=True)
+    latest_tick = pd.read_json(latest_tick, convert_dates=True)
+    market_ticks.append(latest_tick)
+
+    market_ticks['T'] = pd.to_datetime(market_ticks['T'], utc=True)
 
     # return (int(signal), int(newest_dataset_timestamp))
-    close_prices = dataframe['C'].values
-    low_prices = dataframe['L'].values
-    high_prices = dataframe['H'].values
+    close_prices = market_ticks['C'].values
+    low_prices = market_ticks['L'].values
+    high_prices = market_ticks['H'].values
 
     sma = talib.SMA(close_prices, timeperiod=20)
     stddev = pd.rolling_std(close_prices, window=20)
@@ -23,12 +26,12 @@ def predict_newest(chart_data):
 
     upper_bband = []
     lower_bband = []
-    for i in range(len(dataframe)):
+    for i in range(len(market_ticks)):
         upper_bband.append(sma[i] + (stddev[i] * 2))
         lower_bband.append(sma[i] - (stddev[i] * 2))
 
     signals = []
-    for i in range(len(dataframe)):
+    for i in range(len(market_ticks)):
         if low_prices[i] <= lower_bband[i] and stoch_rsi_k[i] <= 20.:
             decision = DECIDE_TO_BUY
             # decision_str = 'BUY'
@@ -42,7 +45,7 @@ def predict_newest(chart_data):
         signals.append(decision)
 
     newest_signal = signals[-1]
-    retrieval_date_gmt = dataframe['T'].values[-1]
+    retrieval_date_gmt = market_ticks['T'].values[-1]
     last_price = close_prices[-1]
     last_upper_bband = upper_bband[-1]
     last_lower_bband = lower_bband[-1]
